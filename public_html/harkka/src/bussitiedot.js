079c499395907366acca549d39b1c9426aa0f634
johannaly <johanna@lyytinen.org>
2020-03-31 19:46:37 +0300

    added jquery dependency

--- public_html/harkka/src/bussitiedot.js
+++ public_html/harkka/src/bussitiedot.js
@@ -1,5 +1,14 @@
 
+import * as $ from 'jquery';
+
 function linjat(callback) {
+    $.ajax({
+        url: 'http://data.itsfactory.fi/journeys/api/1/lines'
+    }).fail(function() {
+        console.log("fail!");
+    }).done(function(data) {
+        console.log(data);
+    }); 
     callback ([1, 4, 5]);
 }
 
@@ -8,5 +17,5 @@ function sijainti(callback) {
     callback ([{"line-id": 2, "lat": "23.34", "lon": "34.56"}]);
 }
 
-
 export {linjat, sijainti};
+
